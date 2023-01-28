2f1f35771da38bf623a113997c99ec4a48798d5b
Lukas Zumvorde <lukaszumvorde@web.de>
2023-01-28 13:33:03 +0100

    feat: add links to table of contents

--- plugins/table-of-contents.lisp
+++ plugins/table-of-contents.lisp
@@ -5,27 +5,7 @@
 function tableofcontent() {
   var place = document.querySelector('div.rawinput');
   var heading = document.createElement('h1');
-  heading.textContent = '~A';
-  var list = document.createElement('ul');
-  place.insertBefore(list, place.firstChild);
-  [].forEach.call(document.querySelectorAll('div.rawinput > h1'), function(obj){
-    var listelement = document.createElement('li');
-    listelement.textContent = obj.textContent;
-    list.appendChild(listelement);
-  });
-  place.insertBefore(heading, place.firstChild);
-}
-tableofcontent();
-" heading)))
-
-(defun plugin-table-of-contents-better (&key (heading "Table of Contents"))
-  "Add a titlepage to the beginning of the presentation"
-  (add-to-front *js*
-				(format nil "
-function tableofcontent() {
-  var place = document.querySelector('div.rawinput');
-  var heading = document.createElement('h1');
-  var idcounter = 1;
+  var idcounter = 2;
   heading.textContent = '~A';
   // heading.setAttribute('id',idcounter++);
   var list = document.createElement('ul');
@@ -33,12 +13,14 @@ function tableofcontent() {
   var lastlevel = 0;
   var sublist;
   [].forEach.call(document.querySelectorAll('div.rawinput > h1,h2'), function(obj){
-    // obj.setAttribute('id',idcounter++);
     if(obj.nodeName.toLowerCase() === 'h1'){
       lastlevel = 0;
       var listelement = document.createElement('li');
-      listelement.textContent = obj.textContent;
+      var linkelement = document.createElement('a');
+      linkelement.setAttribute('href', '#' + idcounter++);
+      linkelement.textContent = obj.textContent;
       list.appendChild(listelement);
+      listelement.appendChild(linkelement);
     };
     if(obj.nodeName.toLowerCase() === 'h2'){
       if(lastlevel === 0){
@@ -46,8 +28,11 @@ function tableofcontent() {
         list.appendChild(sublist);
       };
       var listelement = document.createElement('li');
-      listelement.textContent = obj.textContent;
+      var linkelement = document.createElement('a');
+      linkelement.setAttribute('href', '#' + idcounter++);
+      linkelement.textContent = obj.textContent;
       sublist.appendChild(listelement);
+      listelement.appendChild(linkelement);
       lastlevel = 1;
     };
   });
